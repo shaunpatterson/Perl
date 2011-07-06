8e76566b4573e60e8401ad5d1f5605b4b9766971
Shaun Patterson <shaunpatterson@gmail.com>
2011-07-06 16:50:34 -0400

    Created Match package.  Created a few test cases.

--- GenRegex/Match.pm
+++ GenRegex/Match.pm
@@ -1,4 +1,23 @@
 #!/usr/bin/perl
+#
+# Idea heavily influenced by txt2re.com. Unfortunately txt2re.com isn't (or wasn't at the time) open-sourced. 
+#  I was curious how the engine worked... so I built my own.
+#
+# Regular expressions "stolen" from:  (As far as I can tell all these are in the public domain)
+#   - txt2re.com 
+#   - http://net.tutsplus.com/tutorials/other/8-regular-expressions-you-should-know/
+#   - stackoverflow.com
+#
+# References:
+#   - Regular Expressions Pocket Reference by Tony Stubblebine
+#   - Mastering Regular Expressions by Jeffrey Friedl
+#
+# Shaun Patterson, 2011
+# shaunpatterson@gmail.com
+#
+# Code is released into the public domain. Do with it as you please.
+#
+package Match;
 
 use Location;
 
@@ -10,6 +29,27 @@ unless (caller) {
     main();
 }
 
+sub getGeneralMappings {
+    # Encode the mapping precedence by offset within a matching array
+    my @mappingsList = (
+                            { "char" => [ "."  ] },
+                            { "ws"   => [ "\\s+" ] },
+                            { "var"  => [ "(?:[a-z][a-z0-9_]*)" ] },
+                            { "word" => [ "[a-z]+" ] },
+                            { "day"  => [ "(?:(?:[0-2]?\\d{1})|(?:[3][01]{1})))(?![\\d]" ] },
+                            { "int"  => [ "[0-9]+" ] },
+                            { "month" => [ "(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)" ] },
+                            { "year"  => [ "(?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d]" ] }, 
+                            { "string" => [ '".*?"' , '\\\'.*?\\\'' ] },
+                            { "username" => [ '\b[a-z][a-z0-9_-]{2,15}\b' ] },
+                            { "ddmmmyyy" => [ "(?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[-:\\/.](?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d]" ] },
+                            { "ipaddress" => [ "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d]" ] },
+                       );
+
+    return @mappingsList;
+}
+
+
 sub findPatternMatches {
     my ($input, $name, $regex) = @_;
 
@@ -18,7 +58,7 @@ sub findPatternMatches {
 
     # Total offset relative to the original input string
     my $totalOffset = 0;
-    while ($input =~ m/$regex/ig) {
+    while ($input =~ m/($regex)/ig) {
         # Store the match before we go screwing with the input string
         my $matchedString = $1;
 
@@ -60,9 +100,9 @@ sub buildRegex (\@\@) {
             # Regex selection
             my $location = @$matchedLocations[$selection];
             if ($location->getStart() > $lastEnd) {
-                $regEx .= ".*";
+                $regEx .= ".*";    # Non-greedy
             }
-            $regEx .= $location->getRegex();
+            $regEx .= "(" . $location->getRegex() . ")" ;
             
             $lastEnd = $location->getEnd() + 1;
         
@@ -130,31 +170,34 @@ sub findMatches ($\@) {
     return @matchedLocations;
 }
 
+sub printLocations (\@) {
+    my ($matchedLocations) = @_;
+
+    my $i = 0;
+    foreach my $location (@$matchedLocations) {
+        print "$i: " . $location->toString() . "\n";
+        $i++;
+    }
+}
+
 
 sub main {
-    my $testCase1 = "This is a test";
+    #my $testCase1 = "05:Jul:2011 \'This is an Example!\' 121.1.2.3";
+    my $testCase1 = "sxp1309";
 
     # Encode the mapping precedence by offset within a matching array
-    my @mappingsList = (
-                            { "char" => [ "([a-z])"  ] },
-                            { "word" => [ "([a-z]+)" ] },
-                            { "int"  => [ "([0-9]+)" ] },
-                       );
+    my @mappingsList = getGeneralMappings();
 
     # Hash{start of match} = [ Location Location Location ... ]
     my @matchedLocations = findMatches ($testCase1, @mappingsList);
 
-    my $i = 0;
-    foreach my $location (@matchedLocations) {
-        print "$i: " . $location->toString() . "\n";
-        $i++;
-    }
+
+    printLocations (@matchedLocations);
 
     # Simulated selections
     # Negative selections indicate match the full given text
-    my @userSelections = ( -11, 12, 13, 14 );
+    my @userSelections = ( 11 );
 
-    
     my $regEx = buildRegex (@matchedLocations, @userSelections);
     print $regEx . "\n";
 
@@ -168,3 +211,5 @@ sub main {
     }
 
 }
+
+1;
--- GenRegex/test1.pl
+++ GenRegex/test1.pl
@@ -1,6 +1,92 @@
 #!/usr/bin/perl
+package Test1;
 
 use strict;
 use warnings;
 
-do "match.pl";
+use Match;
+
+unless (caller) {
+    main();
+}
+
+sub test1 {
+    my $testCase = "This is an example";
+
+    # Custom mapping for this test
+    my @mappingsList = (
+                            { "char" => [ "."  ] },
+                            { "word" => [ "[a-z]+" ] },
+                        );
+
+    my @matchedLocations = Match::findMatches ($testCase, @mappingsList);
+   
+    if (scalar @matchedLocations != 22) {
+        return 0;
+    }
+
+    my @userSelections = ( 18, 19, 20, 21 );
+    my $regEx = Match::buildRegex (@matchedLocations, @userSelections);
+    print "$regEx\n";
+
+    unless ("Im gonna need about treefitty" =~ m/$regEx/) {
+        return 0;
+    }
+
+    if ("1234 blah BLAH 567" =~ m/$regEx/) {
+        # Currently failing.  Better regex generation needed
+        return 0;
+    }
+
+    return 1;
+}
+
+sub test2 {
+    my $testCase = "This is an example";
+
+    # Custom mapping for this test
+    my @mappingsList = (
+                            { "char" => [ "."  ] },
+                            { "ws"   => [ "\\s+" ] },
+                            { "word" => [ "[a-z]+" ] },
+                        );
+
+    my @matchedLocations = Match::findMatches ($testCase, @mappingsList);
+   
+    if (scalar @matchedLocations != 25) {
+        return 0;
+    }
+
+    my @userSelections = ( -21, 18, 22, 19, 23, 20, 24 );
+    my $regEx = Match::buildRegex (@matchedLocations, @userSelections);
+    print "$regEx\n";
+
+    unless ("This xxx yyy zzz" =~ m/$regEx/) {
+        return 0;
+    }
+
+    if ("Im gonna need about treefitty" =~ m/$regEx/) {
+        return 0;
+    }
+
+    return 1;
+}
+
+sub test3 {
+    
+}
+
+
+sub main {
+    my $test = "11-27-1983";
+
+    if ($test =~ m/(?:(\d+)-(\d+)-(\d+))/) {
+        print "Match! $1 $2\n";
+    }
+
+    # All test cases should return non-zero if passed
+#    test1() || print "Failed test1\n";
+#    test2() || print "Failed test2\n";
+}
+
+1;
